385db660d4745a2265dc3400611baef353213c2a
monica <monibrave1@gmail.com>
2023-07-19 18:41:51 -0300

    add some explanations

---
+++ start.py
@@ -0,0 +1,14 @@
+#<literales> = 1, 'abc', 2.0, True
+#<operadores> = +, -,*,**,%, ==, =
+#<literal> <operador> <literal>
+
+'''
+>>> 1 + 2
+>>> 1 3.0 #error sintactico
+>>> 5 / 'abc' #error semantico estatico
+>>> 5 * 'abc' #error semantico estatico
+
+#statement o enunciado
+>>> print(1 + 2)
+>>> print('Hello, world!')
+'''
